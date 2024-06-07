451a5d91ec1dd3b6bf0fe0284309a77b63c8db03
Petri Savolainen <petri.savolainen@spaceify.net>
2024-06-07 08:42:17 +0300

    enable linting

--- install-prerequisities.sh
+++ install-prerequisities.sh
@@ -7,8 +7,11 @@ git submodule add https://github.com/microsoft/vcpkg.git
 git submodule add https://github.com/lljbash/clangd-tidy.git
 git submodule update --init --recursive
 
+TEMP_PROFILE_CONTENTS=""
+
 if [[ "$OSTYPE" == "darwin"* ]]; then    
-    PROFILE_FILE=~/.zprofile
+    #PROFILE_FILE=~/.zprofile
+    PROFILE_FILE=/tmp/testfile
     xcode-select --install
     brew install jq
     brew install llvm
@@ -18,23 +21,28 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     ln -s /opt/homebrew/Cellar/llvm/18.1.6/bin/clangd /opt/homebrew/bin/clangd
     brew install trunk
 
-    cd clangd-tidy
     rm -f /opt/homebrew/bin/clang-tidy
-    ln -s clangd-tidy /opt/homebrew/bin/clang-tidy
-    cd ..
+
 else
     PROFILE_FILE=~/.profile
     sudo apt-add-repository http://apt.llvm.org/jammy/llvm-toolchain-jammy main
     sudo apt-get update
     sudo apt-get install -y build-essential cmake ninja-build jq clang-format
     
-    if [[ ":$PATH:" != *":clangd-tidy:"* ]]; then
-        cd clangd-tidy
-        rm -f clang-tidy
-        ln -s clangd-tidy clang-tidy
-        export PATH="$(pwd)/clangd-tidy:$PATH"
-        echo 'export PATH=$(pwd)/clangd-tidy:$PATH' >> $PROFILE_FILE
-    fi
+    curl https://get.trunk.io -fsSL | bash -s -- -y
+fi
+
+cd clangd-tidy
+rm -f clang-tidy
+ln -s clangd-tidy clang-tidy
+cd ..
+
+TEMP_PROFILE_CONTENTS+="export PATH=$(pwd)/clangd-tidy:\$PATH\n"
+
+CLANGD_TIDY_PATH="$(pwd)/clangd-tidy"
+
+if [[ ":$PATH:" != *":$CLANGD_TIDY_PATH:"* ]]; then
+    export PATH="$CLANGD_TIDY_PATH:$PATH"
 fi
 
 cd vcpkg
@@ -44,24 +52,18 @@ cd ..
 export VCPKG_ROOT=$(pwd)/vcpkg
 
 if [[ ":$PATH:" != *":$VCPKG_ROOT:"* ]]; then
-    export PATH="$VCPKG_ROOT:$PATH"
-    echo 'export PATH=$VCPKG_ROOT:$PATH' >> $PROFILE_FILE
+    export PATH=$PATH:$VCPKG_ROOT
 fi
 
-# Check if the VCPKG_ROOT environment variable is already set in the profile file. 
-# If it is, update it. If not, add it.
+# Add VCPKG_ROOT environment variable
+TEMP_PROFILE_CONTENTS+="export VCPKG_ROOT=$(pwd)/vcpkg\n"
 
-if grep -q 'export VCPKG_ROOT=' $PROFILE_FILE; then
-    sed -i '' 's|export VCPKG_ROOT=.*|export VCPKG_ROOT=$(pwd)/vcpkg|' $PROFILE_FILE
-else
-    echo 'export VCPKG_ROOT=$(pwd)/vcpkg' >> $PROFILE_FILE
-fi
+# Add PATH environment variable to include VCPKG_ROOT
+TEMP_PROFILE_CONTENTS+="export PATH=\$VCPKG_ROOT:\$PATH\n"
 
-# Check if the PATH environment variable is already set to include VCPKG_ROOT in the profile file. 
-# If it is, update it. If not, add it.
+echo $TEMP_PROFILE_CONTENTS
 
-if grep -q 'export PATH=$VCPKG_ROOT' $PROFILE_FILE; then
-    sed -i '' 's|export PATH=$VCPKG_ROOT.*|export PATH=$VCPKG_ROOT:$PATH|' $PROFILE_FILE
-else
-    echo 'export PATH=$VCPKG_ROOT:$PATH' >> $PROFILE_FILE
-fi
+# Remove old version of the block
+sed -i.bak '/# streamr-native-sdk added start/,/# streamr-native-sdk added end/d' $PROFILE_FILE
+# Add new block to profile file
+echo -e "# streamr-native-sdk added start\n$TEMP_PROFILE_CONTENTS# streamr-native-sdk added end" >> $PROFILE_FILE
---
+++ lint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+trunk check --no-fix
