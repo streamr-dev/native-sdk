c6ae4e0ffea2e9283333c0fc8a5c11565ddf9327
Petri Savolainen <petri.savolainen@spaceify.net>
2024-06-08 09:10:19 +0300

    report test failure in ci

--- test.sh
+++ test.sh
@@ -4,11 +4,18 @@ cd build
 cmake ..
 make
 
+export GTEST_COLOR=1
+
 if [ "$#" -gt 0 ]; then
     ctest -V -R "$@"
 else
     ctest -V
 fi
 
+CTEST_RETURN_CODE=$?
+
 cd ..
+
+return $CTEST_RETURN_CODE
+
 
