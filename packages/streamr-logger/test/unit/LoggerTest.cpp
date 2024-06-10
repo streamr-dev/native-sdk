b68d90f66a9acff47830055780784cc819e704d3
Santtu <santtu.rantanen@gmail.com>
2024-06-10 18:45:34 +0300

    New tests added

--- packages/streamr-logger/test/unit/LoggerTest.cpp
+++ packages/streamr-logger/test/unit/LoggerTest.cpp
@@ -1,109 +1,140 @@
-#include <gtest/gtest.h>
-#include <streamr-logger/Logger.hpp>
-#include <gtest/gtest.h>
-#include <streamr-logger/Logger.hpp>
-#include <folly/logging/LogMessage.h>
-#include <folly/logging/LogCategory.h>
 #include <string>
+#include <gtest/gtest.h>
+#include <streamr-logger/Logger.hpp>
+#include <folly/logging/LogCategory.h>
+#include <folly/logging/LogMessage.h>
 
 class LoggerTest : public testing::Test {};
 
-/*
-TEST_F(LoggerTest, trace) {
-  Logger logger;
-    logger.logFatal("test failure");
-//  logger.logTrace("Message for trace");
-  EXPECT_EQ(1, 2);
-}
-*/
-/*
-TEST_F(LoggerTest, success) {
-    Logger logger;
-    logger.log("test");
-    int jaa = 1;
-}
-
-TEST_F(LoggerTest, trace) {
-  Logger logger;
-  logger.logTrace("Message for trace");
-  EXPECT_EQ(1, 2);
-}
-*/
+using folly::LoggerDB;
+using folly::LogLevel;
+using streamr::logger::Logger;
+using streamr::logger::StreamrLogFormatter;
+using namespace std::chrono;
 
 class StreamrLogFormatterTest : public testing::Test {
+    // using streamr::logger::StreamrLogFormatter;
+    void SetUp() override {
+      const auto ymd2 = year_month_day(2024y, January, 31d);
+      tp = sys_days{ymd2};
+    }
 
- protected:
-
-  StreamrLogFormatter formatter_{};
+   protected:
+    StreamrLogFormatter formatter_{};
+    system_clock::time_point tp;
 };
 
 TEST_F(StreamrLogFormatterTest, traceNoTruncate) {
-  using namespace std::chrono;
-  constexpr auto ymd2 = std::chrono::year_month_day(
-    2024y, std::chrono::January, 31d 
-  );
-  std::chrono::system_clock::time_point tp = std::chrono::sys_days{ymd2};
-  StreamrLogFormatter::StreamrLogMessage msg = {tp, "Filename.cpp", 100, folly::LogLevel::DBG, "Message"};
-  EXPECT_EQ(formatter_.formatMessageInStreamrStyle(msg), "\x1B[90mTRACE\x1B[0m [2024-01-31T02:00:00.0] (Filename.cpp: 100                   ): \x1B[36mMessage\x1B[0m\n");
+    StreamrLogFormatter::StreamrLogMessage msg = {
+        tp, "Filename.cpp", 100, folly::LogLevel::DBG, "Message"};
+    EXPECT_EQ(
+        formatter_.formatMessageInStreamrStyle(msg),
+        "\x1B[90mTRACE\x1B[0m [2024-01-31T02:00:00.0] (Filename.cpp: 100                   ): \x1B[36mMessage\x1B[0m\n");
 }
 
 TEST_F(StreamrLogFormatterTest, traceTruncate) {
-  using namespace std::chrono;
-  constexpr auto ymd2 = std::chrono::year_month_day(
-    2024y, std::chrono::January, 31d 
-  );
-  std::chrono::system_clock::time_point tp = std::chrono::sys_days{ymd2};
-  StreamrLogFormatter::StreamrLogMessage msg = {tp, "1234567890123456789012345678901234567890.cpp", 100, folly::LogLevel::DBG, "Message"};
-  EXPECT_EQ(formatter_.formatMessageInStreamrStyle(msg), "\x1B[90mTRACE\x1B[0m [2024-01-31T02:00:00.0] (1234567890123456789012345678901: 100): \x1B[36mMessage\x1B[0m\n");
+    StreamrLogFormatter::StreamrLogMessage msg = {
+        tp,
+        "1234567890123456789012345678901234567890.cpp",
+        100,
+        folly::LogLevel::DBG,
+        "Message"};
+    EXPECT_EQ(
+        formatter_.formatMessageInStreamrStyle(msg),
+        "\x1B[90mTRACE\x1B[0m [2024-01-31T02:00:00.0] (1234567890123456789012345678901: 100): \x1B[36mMessage\x1B[0m\n");
 }
 
 TEST_F(StreamrLogFormatterTest, debugNoTruncate) {
-  using namespace std::chrono;
-  constexpr auto ymd2 = std::chrono::year_month_day(
-    2024y, std::chrono::January, 31d 
-  );
-  std::chrono::system_clock::time_point tp = std::chrono::sys_days{ymd2};
-  StreamrLogFormatter::StreamrLogMessage msg = {tp, "Filename.cpp", 101010, folly::LogLevel::DBG0, "Message"};
-  EXPECT_EQ(formatter_.formatMessageInStreamrStyle(msg), "\x1B[34mDEBUG\x1B[0m [2024-01-31T02:00:00.0] (Filename.cpp: 101010                ): \x1B[36mMessage\x1B[0m\n");
+    StreamrLogFormatter::StreamrLogMessage msg = {
+        tp, "Filename.cpp", 101010, folly::LogLevel::DBG0, "Message"};
+    EXPECT_EQ(
+        formatter_.formatMessageInStreamrStyle(msg),
+        "\x1B[34mDEBUG\x1B[0m [2024-01-31T02:00:00.0] (Filename.cpp: 101010                ): \x1B[36mMessage\x1B[0m\n");
 }
 
 TEST_F(StreamrLogFormatterTest, infoNoTruncate) {
-  using namespace std::chrono;
-  constexpr auto ymd2 = std::chrono::year_month_day(
-    2024y, std::chrono::January, 31d 
-  );
-  std::chrono::system_clock::time_point tp = std::chrono::sys_days{ymd2};
-  StreamrLogFormatter::StreamrLogMessage msg = {tp, "Filename.cpp", 101010, folly::LogLevel::INFO, "Message"};
-  EXPECT_EQ(formatter_.formatMessageInStreamrStyle(msg), "\x1B[32mINFO\x1B[0m [2024-01-31T02:00:00.0] (Filename.cpp: 101010                ): \x1B[36mMessage\x1B[0m\n");
+    StreamrLogFormatter::StreamrLogMessage msg = {
+        tp, "Filename.cpp", 101010, folly::LogLevel::INFO, "Message"};
+    EXPECT_EQ(
+        formatter_.formatMessageInStreamrStyle(msg),
+        "\x1B[32mINFO\x1B[0m [2024-01-31T02:00:00.0] (Filename.cpp: 101010                ): \x1B[36mMessage\x1B[0m\n");
 }
 
 TEST_F(StreamrLogFormatterTest, warnoNoTruncate) {
-  using namespace std::chrono;
-  constexpr auto ymd2 = std::chrono::year_month_day(
-    2024y, std::chrono::January, 31d 
-  );
-  std::chrono::system_clock::time_point tp = std::chrono::sys_days{ymd2};
-  StreamrLogFormatter::StreamrLogMessage msg = {tp, "Filename.cpp", 101010, folly::LogLevel::WARN, "Message"};
-  EXPECT_EQ(formatter_.formatMessageInStreamrStyle(msg), "\x1B[33mWARN\x1B[0m [2024-01-31T02:00:00.0] (Filename.cpp: 101010                ): \x1B[36mMessage\x1B[0m\n");
+    StreamrLogFormatter::StreamrLogMessage msg = {
+        tp, "Filename.cpp", 101010, folly::LogLevel::WARN, "Message"};
+    EXPECT_EQ(
+        formatter_.formatMessageInStreamrStyle(msg),
+        "\x1B[33mWARN\x1B[0m [2024-01-31T02:00:00.0] (Filename.cpp: 101010                ): \x1B[36mMessage\x1B[0m\n");
 }
 
 TEST_F(StreamrLogFormatterTest, errorNoTruncate) {
-  using namespace std::chrono;
-  constexpr auto ymd2 = std::chrono::year_month_day(
-    2024y, std::chrono::January, 31d 
-  );
-  std::chrono::system_clock::time_point tp = std::chrono::sys_days{ymd2};
-  StreamrLogFormatter::StreamrLogMessage msg = {tp, "Filename.cpp", 101010, folly::LogLevel::ERR, "Message"};
-  EXPECT_EQ(formatter_.formatMessageInStreamrStyle(msg), "\x1B[31mERROR\x1B[0m [2024-01-31T02:00:00.0] (Filename.cpp: 101010                ): \x1B[36mMessage\x1B[0m\n");
+    StreamrLogFormatter::StreamrLogMessage msg = {
+        tp, "Filename.cpp", 101010, folly::LogLevel::ERR, "Message"};
+    EXPECT_EQ(
+        formatter_.formatMessageInStreamrStyle(msg),
+        "\x1B[31mERROR\x1B[0m [2024-01-31T02:00:00.0] (Filename.cpp: 101010                ): \x1B[36mMessage\x1B[0m\n");
 }
 
 TEST_F(StreamrLogFormatterTest, fatalNoTruncate) {
-  using namespace std::chrono;
-  constexpr auto ymd2 = std::chrono::year_month_day(
-    2024y, std::chrono::January, 31d 
-  );
-  std::chrono::system_clock::time_point tp = std::chrono::sys_days{ymd2};
-  StreamrLogFormatter::StreamrLogMessage msg = {tp, "Filename.cpp", 101010, folly::LogLevel::FATAL, "Message"};
-  EXPECT_EQ(formatter_.formatMessageInStreamrStyle(msg), "\x1B[1;41mFATAL\x1B[0m [2024-01-31T02:00:00.0] (Filename.cpp: 101010                ): \x1B[36mMessage\x1B[0m\n");
+    StreamrLogFormatter::StreamrLogMessage msg = {
+        tp, "Filename.cpp", 101010, folly::LogLevel::FATAL, "Message"};
+    EXPECT_EQ(
+        formatter_.formatMessageInStreamrStyle(msg),
+        "\x1B[1;41mFATAL\x1B[0m [2024-01-31T02:00:00.0] (Filename.cpp: 101010                ): \x1B[36mMessage\x1B[0m\n");
 }
 
+TEST_F(LoggerTest, initialization) {
+    auto logger = Logger::get();
+}
+
+TEST_F(LoggerTest, NoLogLevelEnvVariableSet) {
+    auto logger = Logger::get();
+    unsetenv("LOG_LEVEL");
+    // No log message written
+    EXPECT_EQ(logger.log(LogLevel::INFO, "Testi"), false);
+}
+
+TEST_F(LoggerTest, LogLevelSetWronglyInEnvVariable) {
+    auto logger = Logger::get();
+    setenv("LOG_LEVEL", "NotFound", 1);
+    // No log message written
+    EXPECT_EQ(logger.log(LogLevel::INFO, "Testi"), false);
+}
+
+TEST_F(LoggerTest, LogLevelEnvVariableSetToLowestLevel) {
+    auto logger = Logger::get();
+    setenv("LOG_LEVEL", "trace", 1);
+    // Log written
+    EXPECT_EQ(logger.log(LogLevel::DBG, "Testi"), true);
+}
+
+TEST_F(LoggerTest, RootLogLevelSetToHigherNewValue) {
+    auto logger = Logger::get();
+    setenv("LOG_LEVEL", "trace", 1);
+    setenv("LOG_LEVEL", "fatal", 1);
+    // Log not written
+    EXPECT_EQ(logger.log(LogLevel::INFO, "Testi"), true);
+}
+
+TEST_F(LoggerTest, LoggerDBIntializationWithDefaultLogMinLevel) {
+    LoggerDB db{LoggerDB::TESTING};
+    Logger logger{Logger(db)};
+
+    // Check that minimum root log level set
+    EXPECT_EQ(db.getCategory("")->getLevel(), LogLevel::MIN_LEVEL);
+}
+
+TEST_F(LoggerTest, LoggerDBIntializationWithStreamrHandler) {
+    LoggerDB db{LoggerDB::TESTING};
+    Logger logger{Logger(db)};
+    // Check that minimum root log level set
+    EXPECT_EQ(db.getCategory("")->getHandlers().size(), 1);
+}
+
+TEST_F(LoggerTest, LoggerDBIntializationWithoutStreamrHandler) {
+    LoggerDB db{LoggerDB::TESTING};
+    Logger logger{Logger(db, false)};
+    // Check that minimum root log level set
+    EXPECT_EQ(db.getCategory("")->getHandlers().size(), 0);
+}
