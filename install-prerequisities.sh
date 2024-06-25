e772c94ac576285a1f3c3a9f6622ac3b75b01b88
Petri Savolainen <petri.savolainen@spaceify.net>
2024-06-25 06:50:24 +0300

    use latest homebrew clang on mac, create setenvs.sh file in install-prerequisities

--- install-prerequisities.sh
+++ install-prerequisities.sh
@@ -3,27 +3,29 @@
 # This script needs to be run using "source install-prerequisities.sh"
 # Calling it directly will not work
 
-# git submodule add --force https://github.com/microsoft/vcpkg.git
-# git submodule add --force https://github.com/lljbash/clangd-tidy.git
 git submodule update --init --recursive
 
 TEMP_PROFILE_CONTENTS=""
 
 if [[ "$OSTYPE" == "darwin"* ]]; then    
     #PROFILE_FILE=~/.zprofile
-    PROFILE_FILE=/tmp/testfile
+    PROFILE_FILE=./setenvs.sh
+    
     xcode-select --install
     brew install jq
     brew install llvm
     brew install cmake
     brew install pkg-config
-    ln -s /opt/homebrew/Cellar/llvm/18.1.6/bin/clang-format /opt/homebrew/bin/clang-format
-    ln -s /opt/homebrew/Cellar/llvm/18.1.6/bin/clangd /opt/homebrew/bin/clangd
    
+    rm -f /opt/homebrew/bin/clang-format
+    rm -f /opt/homebrew/bin/clangd
+    ln -s /opt/homebrew/opt/llvm/bin/clang-format /opt/homebrew/bin/clang-format
+    ln -s /opt/homebrew/opt/llvm/bin/clangd /opt/homebrew/bin/clangd
     rm -f /opt/homebrew/bin/clang-tidy
 
 else
-    PROFILE_FILE=~/.profile
+    #PROFILE_FILE=~/.profile
+    PROFILE_FILE=./setenvs.sh
     wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
     sudo apt-add-repository 'deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy main'
     sudo apt-get update
@@ -71,7 +73,7 @@ TEMP_PROFILE_CONTENTS+="export PATH=\$VCPKG_ROOT:\$PATH\n"
 echo $TEMP_PROFILE_CONTENTS
 
 # Remove old version of the block
-sed -i.bak '/# streamr-native-sdk added start/,/# streamr-native-sdk added end/d' $PROFILE_FILE
+sed -i '/# streamr-native-sdk added start/,/# streamr-native-sdk added end/d' $PROFILE_FILE
 # Add new block to profile file
 echo -e "# streamr-native-sdk added start\n$TEMP_PROFILE_CONTENTS# streamr-native-sdk added end" >> $PROFILE_FILE
 
