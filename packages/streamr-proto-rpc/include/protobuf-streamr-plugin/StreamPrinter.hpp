ecfab3dab301e0f2c0fc9086953b3d42f81b27c0
Santtu <santtu.rantanen@gmail.com>
2024-07-19 18:35:15 +0300

    Plugin creates Client proto file now

--- packages/streamr-proto-rpc/include/protobuf-streamr-plugin/StreamPrinter.hpp
+++ packages/streamr-proto-rpc/include/protobuf-streamr-plugin/StreamPrinter.hpp
@@ -5,36 +5,39 @@
 
 namespace streamr::protorpc {
 
-// Logic copied from https://github.com/end2endzone/protobuf-pbop-plugin/blob/master/src/protobuf-pbop-plugin/StreamPrinter.cpp
+// Logic copied from
+// https://github.com/end2endzone/protobuf-pbop-plugin/blob/master/src/protobuf-pbop-plugin/StreamPrinter.cpp
+// Changed size_t to int64_t
 class StreamPrinter {
 private:
     google::protobuf::io::ZeroCopyOutputStream* mStream;
 
 public:
-    StreamPrinter(google::protobuf::io::ZeroCopyOutputStream* iStream)
+    explicit StreamPrinter(google::protobuf::io::ZeroCopyOutputStream* iStream)
         : mStream(iStream) {}
 
-    void print(const unsigned char* iValue, size_t iLength) {
-        size_t dumpIndex = 0;
+    void print(const unsigned char* iValue, int64_t iLength) {
+        int64_t dumpIndex = 0;
 
         while (dumpIndex < iLength) {
             // get a buffer from the stream
-            void* buffer = NULL;
+            void* buffer = nullptr;
             int bufferSize = 0;
             mStream->Next(&buffer, &bufferSize);
 
             if (buffer && bufferSize > 0) {
                 // compute dumpsize
-                size_t dumpSize = iLength - dumpIndex;
-                if ((int)dumpSize > bufferSize)
+                int64_t dumpSize = iLength - dumpIndex;
+                if (dumpSize > bufferSize) {
                     dumpSize = bufferSize;
+                }
 
                 // dump
                 const unsigned char* dumpData = &iValue[dumpIndex];
                 memcpy(buffer, dumpData, dumpSize);
 
-                if ((int)dumpSize < bufferSize) {
-                    int backupSize = bufferSize - dumpSize;
+                if (dumpSize < bufferSize) {
+                    int backupSize = bufferSize - dumpSize; // NOLINT
                     mStream->BackUp(backupSize);
                 }
 
