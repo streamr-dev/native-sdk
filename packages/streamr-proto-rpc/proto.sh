53de96453304b7c91691fcfdd5ffbb4f0b10f510
Santtu <santtu.rantanen@gmail.com>
2024-07-29 15:14:31 +0300

    proto.sh added

--- packages/streamr-proto-rpc/proto.sh
+++ packages/streamr-proto-rpc/proto.sh
@@ -2,5 +2,6 @@ mkdir -p ./src/proto
 protoc -I=protos --cpp_out=src/proto protos/ProtoRpc.proto --experimental_allow_proto3_optional
 
 mkdir -p ./test/proto
+protoc -I=test/protos --cpp_out=./test/proto test/protos/*.proto --experimental_allow_proto3_optional
 protoc --plugin=protoc-gen-streamr=./build/protobuf-streamr-plugin --streamr_out=./test/proto --proto_path=test/protos test/protos/*.proto --experimental_allow_proto3_optional
 
