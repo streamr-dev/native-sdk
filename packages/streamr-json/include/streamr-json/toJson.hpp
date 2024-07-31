3f253082bc2356c79fc7591f8d4bae16835988f3
Petri Savolainen <petri.savolainen@spaceify.net>
2024-07-31 10:10:34 +0300

    fix typos

--- packages/streamr-json/include/streamr-json/toJson.hpp
+++ packages/streamr-json/include/streamr-json/toJson.hpp
@@ -10,13 +10,8 @@
 #include <boost/pfr/traits.hpp>
 #include <boost/pfr/tuple_size.hpp>
 #include <nlohmann/json.hpp>
-<<<<<<< HEAD
-// #include <streamr-json/JsonBuilder.hpp>
-#include <streamr-json/jsonConcepts.hpp>
-=======
 
 #include "streamr-json/jsonConcepts.hpp"
->>>>>>> module1
 
 namespace streamr::json {
 
