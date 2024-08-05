404e488964b43c59f94b7c038c6395e5ec5cb126
Santtu <santtu.rantanen@gmail.com>
2024-08-05 16:21:11 +0300

    Unit tests added

--- packages/streamr-proto-rpc/test/unit/ServerRegistryTest.cpp
+++ packages/streamr-proto-rpc/test/unit/ServerRegistryTest.cpp
@@ -31,17 +31,14 @@ protected:
 
 TEST_F(ServerRegistryTest, TestCanBindMethods) {
     ServerRegistry registry;
-
     HelloRequest request;
     request.set_myname("testUser");
-
     RpcMessage requestWrapper;
     auto& header = *requestWrapper.mutable_header();
     header["method"] = "sayHello";
     header["request"] = "request";
     requestWrapper.mutable_body()->PackFrom(request);
     requestWrapper.set_requestid("request-id");
-
     registry.registerRpcMethod<HelloRequest, HelloResponse>(
         "sayHello",
         +[](const HelloRequest& request,
@@ -51,12 +48,93 @@ TEST_F(ServerRegistryTest, TestCanBindMethods) {
 
             return response;
         });
-
     SLogger::info("requestWrapper", requestWrapper.header().size());
     auto res = registry.handleRequest(requestWrapper, {});
     HelloResponse helloResponse;
     auto unpacked = res.UnpackTo(&helloResponse);
-
     EXPECT_EQ(helloResponse.greeting(), "hello testUser");
 }
+
+TEST_F(ServerRegistryTest, HandleUnknownRpcMethod) {
+    // Create a test RPC message with an unknown method
+    ServerRegistry registry;
+    HelloRequest request;
+    request.set_myname("testUser");
+    RpcMessage requestWrapper;
+    auto& header = *requestWrapper.mutable_header();
+    header["method"] = "unknownMethod";
+    header["request"] = "request";
+    requestWrapper.mutable_body()->PackFrom(request);
+    requestWrapper.set_requestid("request-id");
+    EXPECT_THROW(registry.handleRequest(requestWrapper, {}), UnknownRpcMethod);
+}
+
+TEST_F(ServerRegistryTest, HandleRequestWithMissingMethodHeader) {
+    // Create a test RPC message with an unknown method
+    ServerRegistry registry;
+    HelloRequest request;
+    request.set_myname("testUser");
+    RpcMessage requestWrapper;
+    auto& header = *requestWrapper.mutable_header();
+    header["request"] = "request";
+    requestWrapper.mutable_body()->PackFrom(request);
+    requestWrapper.set_requestid("request-id");
+    EXPECT_THROW(registry.handleRequest(requestWrapper, {}), UnknownRpcMethod);
+}
+
+TEST_F(ServerRegistryTest, RegisterAndHandleRpcNotification) {
+    ServerRegistry registry;
+    HelloRequest request;
+    request.set_myname("testUser");
+    RpcMessage requestWrapper;
+    auto& header = *requestWrapper.mutable_header();
+    header["method"] = "testNotification";
+    header["request"] = "request";
+    requestWrapper.mutable_body()->PackFrom(request);
+    requestWrapper.set_requestid("request-id");
+    bool notificationCalled = false;
+    auto rpcNotification = [&notificationCalled](
+                               const HelloRequest& /* request */,
+                               const ProtoCallContext& /* context */) -> void {
+        notificationCalled = true;
+    };
+    // Register the RPC notification
+    registry.registerRpcNotification<HelloRequest>(
+        "testNotification", rpcNotification);
+    registry.handleNotification(requestWrapper, {});
+    // Verify the notification was called
+    EXPECT_TRUE(notificationCalled);
+}
+
+TEST_F(ServerRegistryTest, HandleUnknownRpcNotification) {
+    // Create a test RPC message with an unknown notification
+    ServerRegistry registry;
+    HelloRequest request;
+    request.set_myname("testUser");
+    RpcMessage requestWrapper;
+    auto& header = *requestWrapper.mutable_header();
+    header["method"] = "unknownNotification";
+    header["request"] = "request";
+    requestWrapper.mutable_body()->PackFrom(request);
+    requestWrapper.set_requestid("request-id");
+    // Expect an exception to be thrown
+    EXPECT_THROW(
+        registry.handleNotification(requestWrapper, {}), UnknownRpcMethod);
+}
+
+TEST_F(ServerRegistryTest, HandleNotificationWithMissingMethodHeader) {
+    // Create a test RPC message without a method header
+    ServerRegistry registry;
+    HelloRequest request;
+    request.set_myname("testUser");
+    RpcMessage requestWrapper;
+    auto& header = *requestWrapper.mutable_header();
+    header["request"] = "request";
+    requestWrapper.mutable_body()->PackFrom(request);
+    requestWrapper.set_requestid("request-id");
+    // Expect an exception to be thrown
+    EXPECT_THROW(
+        registry.handleNotification(requestWrapper, {}), UnknownRpcMethod);
+}
+
 } // namespace streamr::protorpc
