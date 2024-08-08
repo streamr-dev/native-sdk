30872009f4e05dbfa233242d4378752185d45138
Petri Savolainen <petri.savolainen@spaceify.net>
2024-08-09 02:29:06 +0300

    fix linting in CI

--- packages/streamr-proto-rpc/include/streamr-proto-rpc/ProtoCallContext.hpp
+++ packages/streamr-proto-rpc/include/streamr-proto-rpc/ProtoCallContext.hpp
@@ -2,11 +2,13 @@
 #define STREAMR_PROTO_RPC_PROTO_CALL_CONTEXT_HPP
 
 #include <map>
+#include <optional>
+#include <string>
 
 namespace streamr::protorpc {
 
 struct ProtoCallContext : public std::map<std::string, std::string> {
-    std::optional<size_t> timeout;
+    std::optional<std::size_t> timeout;
     bool notification = false;
 };
 
