7e2f0e4bc99f88db3875b7cd580df0c930135e91
Petri Savolainen <petri.savolainen@spaceify.net>
2024-08-09 10:37:50 +0300

    add example routedhello

--- packages/streamr-proto-rpc/examples/routed-hello/routedhello.cpp
+++ packages/streamr-proto-rpc/examples/routed-hello/routedhello.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string_view>
+#include <map>
 #include <folly/coro/BlockingWait.h>
 #include "RoutedHelloRpc.client.pb.h"
 #include "RoutedHelloRpc.pb.h"
@@ -6,73 +8,182 @@
 #include "streamr-proto-rpc/ProtoCallContext.hpp"
 #include "streamr-proto-rpc/RpcCommunicator.hpp"
 
+using streamr::protorpc::ProtoCallContext;
+using ::RoutedHelloResponse;
 using streamr::protorpc::RoutedHelloRpcService;
 using streamr::protorpc::RoutedHelloRpcServiceClient;
-using streamr::protorpc::ProtoCallContext;
 using streamr::protorpc::RpcCommunicator;
 using streamr::protorpc::RpcMessage;
-
 class RoutedHelloService : public RoutedHelloRpcService {
-public:
+private:
     std::string mServiceId;
 
-    RoutedHelloService(const std::string& serviceId) : mServiceId(serviceId) {}
+public:
+    explicit RoutedHelloService(std::string_view serviceId)
+        : mServiceId(std::string(serviceId)) {}
 
     RoutedHelloResponse sayHello(
         const RoutedHelloRequest& request,
         const ProtoCallContext& callContext) override {
         RoutedHelloResponse response;
-        std::string sourceId = callContext.at("sourceId");   
-        response.set_greeting("Hello, " + request.myname());
+        std::string sourceId = callContext.at("sourceId");
+        std::cout << "sayHello() called on server " << mServiceId
+                  << " with context parameter sourceId " << sourceId << "\n";
+        response.set_greeting("Hello, " + request.myname() + "!");
         return response;
     }
+    [[nodiscard]] std::string getServiceId() const { return mServiceId; }
 };
 
 int main() {
-    std::unordered_map<std::string, RpcCommunicator> clientCommunicators;
-    clientCommunicators.emplace("1", RpcCommunicator());
-   // auto& communicator1 = clientCommunicators.at("1");
-   // clientCommunicators["1"] = 
-    // Setup server 1
-  //  RpcCommunicator communicator1; 
-    RoutedHelloService helloService1("1");
-    communicator1.registerRpcMethod<RoutedHelloRequest, RoutedHelloResponse>(
-        "sayHello",
-        std::bind( // NOLINT(modernize-avoid-bind)
-            &RoutedHelloService::sayHello,
-            &helloService1,
-            std::placeholders::_1,
-            std::placeholders::_2));
-
-    // Setup server 2
-    RpcCommunicator communicator2; 
-    RoutedHelloService helloService2("2");
-    communicator2.registerRpcMethod<RoutedHelloRequest, RoutedHelloResponse>(
-        "sayHello",
-        std::bind( // NOLINT(modernize-avoid-bind)
-            &RoutedHelloService::sayHello,
-            &helloService2,
-            std::placeholders::_1,
-            std::placeholders::_2));
-
-    // Setup client 1
-    RoutedHelloRpcServiceClient helloClient1(communicator1);
-  //  clientCommunicators["Client1"] = std::move(communicator1)
-  //  clientCommunicators["1"] = communicator1;
-
-/*
+    std::map<std::string, std::shared_ptr<RpcCommunicator>>
+        clientCommunicators;
 
     // Setup server
-    RpcCommunicator communicator1;
-    HelloService helloService;
+    
+    RpcCommunicator serverCommunicator1;
+    RoutedHelloService helloService1("1");
 
-    communicator1.registerRpcMethod<HelloRequest, HelloResponse>(
-        "sayHello",
-        std::bind( // NOLINT(modernize-avoid-bind)
-            &HelloService::sayHello,
-            &helloService,
-            std::placeholders::_1,
-            std::placeholders::_2));  
-   */
+    serverCommunicator1
+        .registerRpcMethod<RoutedHelloRequest, RoutedHelloResponse>(
+            "sayHello",
+            std::bind( // NOLINT(modernize-avoid-bind)
+                &RoutedHelloService::sayHello,
+                &helloService1,
+                std::placeholders::_1,
+                std::placeholders::_2));
+
+    // Setup server 2
+    
+    RpcCommunicator serverCommunicator2;
+    RoutedHelloService helloService2("2");
+
+    serverCommunicator2
+        .registerRpcMethod<RoutedHelloRequest, RoutedHelloResponse>(
+            "sayHello",
+            std::bind( // NOLINT(modernize-avoid-bind)
+                &RoutedHelloService::sayHello,
+                &helloService2,
+                std::placeholders::_1,
+                std::placeholders::_2));
+
+    // Setup client1
+    
+    auto communicator1 = std::make_shared<RpcCommunicator>();
+    RoutedHelloRpcServiceClient helloClient1(*communicator1);
+    clientCommunicators["1"] = communicator1;
+
+    // Setup client2
+    
+    auto communicator2 = std::make_shared<RpcCommunicator>();
+    RoutedHelloRpcServiceClient helloClient2(*communicator2);
+    clientCommunicators["2"] = communicator2;
+
+    // Simulate a network connections
+    
+    serverCommunicator1.setOutgoingMessageCallback(
+        [&clientCommunicators](
+            const RpcMessage& message,
+            const std::string& /* requestId */,
+            const ProtoCallContext& context) -> void {
+            // Send the reply message to the calling client based on
+            // sourceId passed
+            // through the network stack in the context information
+            const std::string sourceId = context.at("sourceId");
+            clientCommunicators[sourceId]->handleIncomingMessage(
+                message, context);
+        });
+
+    serverCommunicator2.setOutgoingMessageCallback(
+        [&clientCommunicators](
+            const RpcMessage& message,
+            const std::string& /* requestId */,
+            const ProtoCallContext& context) -> void {
+            const std::string sourceId = context.at("sourceId");
+            clientCommunicators[sourceId]->handleIncomingMessage(
+                message, context);
+        });
+
+    communicator1->setOutgoingMessageCallback(
+        [&clientCommunicators, &serverCommunicator1, &serverCommunicator2](
+            const RpcMessage& message,
+            const std::string& /* requestId */,
+            const ProtoCallContext& context) -> void {
+            // Choose the server to send the message to based on context
+            // information passed through the RPC stack as client context
+            // information
+            RpcCommunicator* server;
+            std::string targetServerId = context.at("targetServerId");
+
+            if (targetServerId == "2") {
+                server = &serverCommunicator2;
+            } else {
+                server = &serverCommunicator1;
+            }
+
+            // Here you would transport the msgBody over network to the server
+            // ...
+            // At the server you would pass the id of the calling client as
+            // context information to the server. The server is free to choose
+            // what to use as the id of the calling client; it might use, for
+            // example, the id of the network socket, something negotiated
+            // during connection handshake, or something passed on in every
+            // network payload.
+            //
+            // The context information gets passed uncahged through the RPC
+            // stack, so the reply message can be routed to the correct client.
+
+            ProtoCallContext serverContext;
+            serverContext["sourceId"] = "1";
+
+            server->handleIncomingMessage(message, serverContext);
+        });
+
+    communicator2->setOutgoingMessageCallback(
+        [&clientCommunicators, &serverCommunicator1, &serverCommunicator2](
+            const RpcMessage& message,
+            const std::string& /* requestId */,
+            const ProtoCallContext& context) -> void {
+            RpcCommunicator* server;
+            std::string targetServerId = context.at("targetServerId");
+
+            if (targetServerId == "2") {
+                server = &serverCommunicator2;
+            } else {
+                server = &serverCommunicator1;
+            }
+
+            ProtoCallContext serverContext;
+            serverContext["sourceId"] = "2";
+
+            server->handleIncomingMessage(message, serverContext);
+        });
+
+    // Make some RPC calls
+
+    RoutedHelloRequest request;
+    request.set_myname("Alice");
+    ProtoCallContext context;
+    context["targetServerId"] = "2";
+
+    auto response =
+        folly::coro::blockingWait(helloClient1.sayHello(request, context));
+
+    std::cout << "Client 1 (Alice) got message from server: " +
+            response.greeting()
+              << "\n";
+
+    RoutedHelloRequest request2;
+    request2.set_myname("Bob");
+    ProtoCallContext context2;
+    context2["targetServerId"] = "1";
+
+    auto response2 =
+        folly::coro::blockingWait(helloClient2.sayHello(request2, context2));
+
+    std::cout << "Client 2 (Bob) got message from server: " +
+            response2.greeting()
+              << "\n";
+
     return 0;
 }
