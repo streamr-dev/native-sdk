e148a8b17966d8247a648caf73ddc3961dfce3c0
Petri Savolainen <petri.savolainen@spaceify.net>
2024-08-19 12:02:42 +0300

    developing install.sh for iphone

--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@
 # Parse command-line options
 PROD_BUILD=false
 TARGET_TRIPLET=""
+CHAINLOAD_TOOLCHAIN_FILE=""
+PLATFORM=""
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --prod) PROD_BUILD=true ;;
-        --ios) TARGET_TRIPLET="arm64-ios" ;;
-        --android) TARGET_TRIPLET="arm64-android" ;;
+        --ios) TARGET_TRIPLET="arm64-ios"; CHAINLOAD_TOOLCHAIN_FILE="$(pwd)/overlaytriplets/arm64-ios.cmake"; PLATFORM="OS64" ;;
+        --android) TARGET_TRIPLET="arm64-android"; CHAINLOAD_TOOLCHAIN_FILE="$(pwd)/overlaytriplets/arm64-android.cmake"; PLATFORM="android" ;;
         *) echo "Unknown parameter passed: $1. Usage: ./install.sh [--prod] [--ios] [--android]"; exit 1 ;;
     esac
     shift
@@ -39,7 +41,7 @@ fi
 for package in $(cat MonorepoPackages.cmake | grep -v "set(MonorepoPackages" | grep -v ")"); do
     cd packages/$package/build
     if [ -n "$TARGET_TRIPLET" ]; then
-        cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DVCPKG_TARGET_TRIPLET=$TARGET_TRIPLET ..
+        cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DVCPKG_TARGET_TRIPLET=$TARGET_TRIPLET -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$CHAINLOAD_TOOLCHAIN_FILE -DPLATFORM=$PLATFORM ..
     else
         cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
     fi
@@ -50,7 +52,7 @@ done
 # Call build for the root project
 echo "Building root project"
 if [ -n "$TARGET_TRIPLET" ]; then
-    cd build && cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DVCPKG_TARGET_TRIPLET=$TARGET_TRIPLET .. && cmake --build . && cd ..
+    cd build && cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DVCPKG_TARGET_TRIPLET=$TARGET_TRIPLET -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$CHAINLOAD_TOOLCHAIN_FILE -DPLATFORM=$PLATFORM .. && cmake --build . && cd ..
 else
     cd build && cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE .. && cmake --build . && cd ..
 fi
