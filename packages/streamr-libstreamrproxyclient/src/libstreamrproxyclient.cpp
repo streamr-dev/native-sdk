5c4b441e13cb508d117c13f1dd2af623c1e9a273
Petri Savolainen <petri.savolainen@spaceify.net>
2024-08-19 11:28:24 +0300

    add support for --ios and --android flags in install.sh

---
+++ packages/streamr-libstreamrproxyclient/src/libstreamrproxyclient.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "libstreamrproxyclient.hpp"
+
+void testRpc() {
+    std::cout << "Hello, World!" << "\n";
+}
--- packages/streamr-proto-rpc/install.sh
+++ packages/streamr-proto-rpc/install.sh
@@ -2,11 +2,14 @@
 
 # Parse command-line options
 PROD_BUILD=false
+TARGET_TRIPLET=""
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --prod) PROD_BUILD=true ;;
-        *) echo "Unknown parameter passed: $1. Usage: ./install.sh [--prod]"; exit 1 ;;
+        --iphone) TARGET_TRIPLET="arm64-ios" ;;
+        --android) TARGET_TRIPLET="arm64-android" ;;
+        *) echo "Unknown parameter passed: $1. Usage: ./install.sh [--prod] [--iphone] [--android]"; exit 1 ;;
     esac
     shift
 done
@@ -18,4 +21,13 @@ else
     BUILD_TYPE="Debug"
 fi
 
-cd build && cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE .. && cmake --build . && cd ..
+# Print the target platform if specified
+if [ -n "$TARGET_TRIPLET" ]; then
+    echo "Target platform: $TARGET_TRIPLET"
+fi
+
+if [ -n "$TARGET_TRIPLET" ]; then
+    cd build && cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DVCPKG_TARGET_TRIPLET=$TARGET_TRIPLET .. && cmake --build . && cd ..
+else
+    cd build && cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE .. && cmake --build . && cd ..
+fi
