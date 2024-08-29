c74c2b820e92ee153a7c11cbd274d473021e53b3
Santtu <santtu.rantanen@gmail.com>
2024-08-29 13:29:13 +0300

    Some cleaning

--- packages/streamr-libstreamrproxyclient/examples/android/LocationShare/app/src/main/java/com/example/locationshare/ui/LocationShareViewModel.kt
+++ packages/streamr-libstreamrproxyclient/examples/android/LocationShare/app/src/main/java/com/example/locationshare/ui/LocationShareViewModel.kt
@@ -17,39 +17,6 @@ import kotlinx.coroutines.flow.update
  */
 class LocationShareViewModel(val streamrProxyClient: StreamrProxyClient) : ViewModel() {
 
-    // Game UI state
-  //  private val _uiState = MutableStateFlow(ProxyState())
-   // val streamrProxyClient = StreamrProxyClient()
-
-
-
-/*
-    fun updateIntervalSeconds(intervalSeconds: String) {
-        val parsedInt = intervalSeconds.toIntOrNull()
-        if (parsedInt != null) {
-            streamrProxyClient.updateIntervalSeconds(parsedInt)
-        } else {
-            streamrProxyClient.updateIntervalSeconds(StreamrProxyClient.defaultPublishingIntervalInSeconds)
-        }
-    }
-
- */
-  //  val uiState: StateFlow<ProxyState> = _uiState.asStateFlow()
-    /*
-    var proxyAddress by mutableStateOf("")
-        private set
-
-    var proxyId by mutableStateOf("")
-        private set
-
-    fun updateProxyAddress(proxyAddress: String){
-        this.proxyAddress = proxyAddress
-    }
-
-    fun updateProxyId(proxyId: String){
-        this.proxyId = proxyId
-    }
-*/
     fun buttonClicked() {
         when (streamrProxyClient.status) {
             Status.stopped -> streamrProxyClient.startPublishing()
