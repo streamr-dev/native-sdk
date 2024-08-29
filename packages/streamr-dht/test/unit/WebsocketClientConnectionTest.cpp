9420a7394a3288e01a8006606abff902e901f2f5
Santtu <santtu.rantanen@gmail.com>
2024-08-29 08:26:49 +0300

    Fix for segmentation fault. And some unit tests which should be modified later when exception handling is added.

--- packages/streamr-dht/test/unit/WebsocketClientConnectionTest.cpp
+++ packages/streamr-dht/test/unit/WebsocketClientConnectionTest.cpp
@@ -1,8 +1,22 @@
 #include <gtest/gtest.h>
 #include "streamr-dht/connection/websocket/WebsocketClientConnection.hpp"
+#include <streamr-dht/utils/Errors.hpp>
 
 using streamr::dht::connection::websocket::WebsocketClientConnection;
 
 TEST(WebsocketClientConnection, TestCanBeCreated) {
     WebsocketClientConnection connection;
+}
+
+TEST(WebsocketClientConnection, TestInvalidAddress) {
+    WebsocketClientConnection connection;
+    connection.connect("InvalidAddress", false);
+}
+
+TEST(WebsocketClientConnection, TestCannotSendDataIfNoConnection) {
+    WebsocketClientConnection connection;
+    std::vector<std::byte> message;
+    message.push_back(std::byte{1});
+    connection.send(message);
+
 }
