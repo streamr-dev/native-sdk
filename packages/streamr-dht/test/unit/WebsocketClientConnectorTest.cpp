3496b0119e7d368bc1d563b98eec6cf0021de20b
Santtu <santtu.rantanen@gmail.com>
2024-09-18 12:08:10 +0700

    Unit tests added

--- packages/streamr-dht/test/unit/WebsocketClientConnectorTest.cpp
+++ packages/streamr-dht/test/unit/WebsocketClientConnectorTest.cpp
@@ -12,6 +12,7 @@ using ::dht::NodeType;
 using ::dht::PeerDescriptor;
 using streamr::dht::DhtAddress;
 using streamr::dht::connection::PendingConnection;
+using streamr::dht::connection::pendingconnectionevents::Disconnected;
 using streamr::dht::connection::websocket::WebsocketClientConnector;
 using streamr::dht::connection::websocket::WebsocketClientConnectorOptions;
 using streamr::dht::transport::ListeningRpcCommunicator;
@@ -28,6 +29,23 @@ public:
     }
 
     void stop() override {}
+};
+
+class DummyConnection : public streamr::dht::connection::Connection {
+public:
+    DummyConnection() : Connection(streamr::dht::connection::ConnectionType::WEBSOCKET_CLIENT) {}
+
+    void send(const std::vector<std::byte>& data) override {
+        std::cout << "DummyConnection: Sending " << data.size() << " bytes" << std::endl;
+    }
+
+    void close(bool gracefulLeave) override {
+        std::cout << "DummyConnection: Closing (graceful: " << (gracefulLeave ? "true" : "false") << ")" << std::endl;
+    }
+
+    void destroy() override {
+        std::cout << "DummyConnection: Destroying" << std::endl;
+    }
 };
 
 class WebsocketClientConnectorTest : public ::testing::Test {
@@ -77,7 +95,7 @@ protected:
 };
 
 TEST_F(
-    WebsocketClientConnectorTest, IsPossibleToFormConnectionNodeWithoutServer) {
+    WebsocketClientConnectorTest, IsPossibleToFormConnection_NodeWithoutServer) {
     connector->setLocalPeerDescriptor(createMockPeerDescriptor("local", false));
     EXPECT_TRUE(connector->isPossibleToFormConnection(
         createMockPeerDescriptor("remote", true)));
@@ -90,7 +108,7 @@ TEST_F(
 }
 
 TEST_F(
-    WebsocketClientConnectorTest, IsPossibleToFormConnectionNodeWithTLSServer) {
+    WebsocketClientConnectorTest, IsPossibleToFormConnection_NodeWithTLSServer) {
     connector->setLocalPeerDescriptor(
         createMockPeerDescriptor("local", true, true));
     EXPECT_TRUE(connector->isPossibleToFormConnection(
@@ -105,7 +123,7 @@ TEST_F(
 
 TEST_F(
     WebsocketClientConnectorTest,
-    IsPossibleToFormConnectionNodeWithNonTLSServer) {
+    IsPossibleToFormConnection_NodeWithNonTLSServer) {
     connector->setLocalPeerDescriptor(
         createMockPeerDescriptor("local", true, false));
     EXPECT_TRUE(connector->isPossibleToFormConnection(
@@ -120,7 +138,7 @@ TEST_F(
 
 TEST_F(
     WebsocketClientConnectorTest,
-    IsPossibleToFormConnectionNodeWithNonTLSServerInLocalNetwork) {
+    IsPossibleToFormConnection_NodeWithNonTLSServerInLocalNetwork) {
     connector->setLocalPeerDescriptor(createMockPeerDescriptor(
         "local", true, false, NodeType::NODEJS, "192.168.11.11"));
     EXPECT_TRUE(connector->isPossibleToFormConnection(
@@ -133,7 +151,7 @@ TEST_F(
         createMockPeerDescriptor("remote", false, false, NodeType::BROWSER)));
 }
 
-TEST_F(WebsocketClientConnectorTest, IsPossibleToFormConnectionBrowser) {
+TEST_F(WebsocketClientConnectorTest, IsPossibleToFormConnection_Browser) {
     connector->setLocalPeerDescriptor(
         createMockPeerDescriptor("local", false, false, NodeType::BROWSER));
     EXPECT_TRUE(connector->isPossibleToFormConnection(
@@ -144,4 +162,38 @@ TEST_F(WebsocketClientConnectorTest, IsPossibleToFormConnectionBrowser) {
         createMockPeerDescriptor("remote", false)));
     EXPECT_FALSE(connector->isPossibleToFormConnection(
         createMockPeerDescriptor("remote", false, false, NodeType::BROWSER)));
+}
+
+TEST_F(WebsocketClientConnectorTest, Connect_ReturnsExistingConnectingConnection) {
+    connector->setLocalPeerDescriptor(
+        createMockPeerDescriptor("local", false));
+    auto remotePeerDescriptor = createMockPeerDescriptor("remote", true);
+    auto firstConnection = connector->connect(remotePeerDescriptor);
+    auto secondConnection = connector->connect(remotePeerDescriptor);
+    EXPECT_TRUE(firstConnection == secondConnection);
+    firstConnection->close(false);
+}
+
+TEST_F(WebsocketClientConnectorTest, Connect_DisconnectedEventRemovesConnectingConnection) {
+    connector->setLocalPeerDescriptor(
+        createMockPeerDescriptor("local", false));
+    auto remotePeerDescriptor = createMockPeerDescriptor("remote", true);
+    auto firstConnection = connector->connect(remotePeerDescriptor);
+    firstConnection->emit<Disconnected>(false); 
+    auto secondConnection = connector->connect(remotePeerDescriptor);
+    EXPECT_FALSE(firstConnection == secondConnection);
+    firstConnection->close(false);
+    secondConnection->close(false);
+}
+
+TEST_F(WebsocketClientConnectorTest, Connect_ConnectedEventRemovesConnectingConnection) {
+    connector->setLocalPeerDescriptor(
+        createMockPeerDescriptor("local", false));
+    auto remotePeerDescriptor = createMockPeerDescriptor("remote", true);
+    auto firstConnection = connector->connect(remotePeerDescriptor);
+    firstConnection->emit<Disconnected>(false); 
+    auto secondConnection = connector->connect(remotePeerDescriptor);
+    EXPECT_FALSE(firstConnection == secondConnection);
+    firstConnection->close(false);
+    secondConnection->close(false);
 }
