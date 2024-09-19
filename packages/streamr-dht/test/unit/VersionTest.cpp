e2b46cfe733a1d7b823a6590aed0a53824b11ee8
Santtu <santtu.rantanen@gmail.com>
2024-09-19 14:23:23 +0700

    Unit tests added

---
+++ packages/streamr-dht/test/unit/VersionTest.cpp
@@ -0,0 +1,31 @@
+
+#include <gtest/gtest.h>
+#include "streamr-dht/helpers/Version.hpp"
+
+using  streamr::dht::helpers::Version;
+
+class VersionTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        // Setup code if needed
+    }
+
+    void TearDown() override {
+        // Teardown code if needed
+    }
+};
+
+TEST_F(VersionTest, Supported) {
+    EXPECT_TRUE(Version::isMaybeSupportedVersion("1.0"));
+    EXPECT_TRUE(Version::isMaybeSupportedVersion("1.1"));
+    EXPECT_TRUE(Version::isMaybeSupportedVersion("2.0"));          
+    EXPECT_TRUE(Version::isMaybeSupportedVersion("3.5"));
+}
+
+TEST_F(VersionTest, NotSupported) {
+    EXPECT_FALSE(Version::isMaybeSupportedVersion(""));
+    EXPECT_FALSE(Version::isMaybeSupportedVersion("100.0.0-testnet-three.3"));
+    EXPECT_FALSE(Version::isMaybeSupportedVersion("0.0"));          
+    EXPECT_FALSE(Version::isMaybeSupportedVersion("0.1"));
+}
+
