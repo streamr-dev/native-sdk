2d07318792aa94b5514144df51c1048ef9e1cee5
Petri Savolainen <petri.savolainen@spaceify.net>
2024-10-02 09:19:55 +0300

    add more placeholder unit tests

---
+++ packages/streamr-utils/test/unit/EthereumAddressTest.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+#include "streamr-utils/EthereumAddress.hpp"
+
+using streamr::utils::EthereumAddress; // NOLINT
+
+TEST(EthereumAddressTest, ItCanBeInstantiated) {
+    // EthereumAddress ethereumAddress;
+}
---
+++ packages/streamr-utils/test/unit/ReplayEventEmitterWrapperTest.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+#include "streamr-utils/ReplayEventEmitterWrapper.hpp"
+
+using streamr::utils::ReplayEventEmitterWrapper; // NOLINT
+
+TEST(ReplayEventEmitterWrapperTest, ItCanBeInstantiated) {
+    // ReplayEventEmitterWrapper replayEventEmitterWrapper;
+}
