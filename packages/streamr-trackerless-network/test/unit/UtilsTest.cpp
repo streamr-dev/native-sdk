3f9854da909acb9d91a1ce0da51c42278c0e8b82
Petri Savolainen <petri.savolainen@spaceify.net>
2024-10-02 09:13:07 +0300

    add placeholder unit tests to fix linting

---
+++ packages/streamr-trackerless-network/test/unit/UtilsTest.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+#include "streamr-trackerless-network/logic/Utils.hpp"
+
+using streamr::trackerlessnetwork::Utils; // NOLINT
+
+TEST(UtilsTest, ItCanBeInstantiated) {
+    // Utils utils;
+}
