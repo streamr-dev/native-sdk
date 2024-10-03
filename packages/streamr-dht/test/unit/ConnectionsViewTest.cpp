83e7e0789e6a0556225ca2bc9f55b29c5905638d
Petri Savolainen <petri.savolainen@spaceify.net>
2024-10-03 09:41:59 +0300

    try fixing linting errors on linux

---
+++ packages/streamr-dht/test/unit/ConnectionsViewTest.cpp
@@ -0,0 +1,6 @@
+#include "streamr-dht/connection/ConnectionsView.hpp"
+#include <gtest/gtest.h>
+
+using streamr::dht::connection::ConnectionsView; // NOLINT
+
+TEST(ConnectionsView, ItCanBeConstructed) {}
