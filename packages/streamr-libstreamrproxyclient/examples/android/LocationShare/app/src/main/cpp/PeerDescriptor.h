1f0eb133cf41e62a36f06a301681c1c5827e9127
Santtu <santtu.rantanen@gmail.com>
2024-10-05 14:03:51 +0700

    Android App added

---
+++ packages/streamr-libstreamrproxyclient/examples/android/LocationShare/app/src/main/cpp/PeerDescriptor.h
@@ -0,0 +1,15 @@
+//
+// Created by Santtu Rantanen on 28.8.2024.
+//
+
+#ifndef LOCATIONSHARE_PEERDESCRIPTOR_H
+#define LOCATIONSHARE_PEERDESCRIPTOR_H
+
+#include <string>
+
+struct PeerDescriptor {
+    std::string peerId;
+    std::string peerAddress;
+};
+
+#endif //LOCATIONSHARE_PEERDESCRIPTOR_H
