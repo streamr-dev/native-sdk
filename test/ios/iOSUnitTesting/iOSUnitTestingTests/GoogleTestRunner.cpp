bbc56aed23527e30aadc648c318903cbbc1c68c6
Santtu <santtu.rantanen@gmail.com>
2024-10-05 19:39:17 +0700

    iOS unit test app added

---
+++ test/ios/iOSUnitTesting/iOSUnitTestingTests/GoogleTestRunner.cpp
@@ -0,0 +1,24 @@
+//
+//  GoogleTestRunner.cpp
+//  iOSUnitTestingTests
+//
+//  Created by Santtu Rantanen on 30.8.2024.
+//
+
+#define GLOG_USE_GLOG_EXPORT
+#define GLOG_EXPORT
+
+#include "GoogleTestRunner.hpp"
+#include "gtest/gtest.h"
+
+GoogleTestResult GoogleTestRunner::runAllTests(int argc, char** argv) const {
+    testing::internal::CaptureStdout();
+    testing::InitGoogleTest(&argc, argv);
+    int result = RUN_ALL_TESTS();
+    std::string output = testing::internal::GetCapturedStdout();
+    GoogleTestResult googleTestResult = {output, result};
+    return googleTestResult;
+}
+
+
+
