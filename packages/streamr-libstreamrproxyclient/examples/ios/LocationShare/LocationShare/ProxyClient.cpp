61886f21dcbfa112a9b56baad44a220fe7c73676
Santtu <santtu.rantanen@gmail.com>
2024-10-05 10:02:23 +0700

    iOS xcframework and iOS LocationShare App (With static lib) added

---
+++ packages/streamr-libstreamrproxyclient/examples/ios/LocationShare/LocationShare/ProxyClient.cpp
@@ -0,0 +1,57 @@
+//
+//  ProxyClient.cpp
+//  LocationShare
+//
+//  Created by Santtu Rantanen on 25.8.2024.
+//
+
+#include "ProxyClient.hpp"
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include "streamr-utils/EthereumAddress.hpp"
+#include "streamr-utils/StreamPartID.hpp"
+#include "streamrproxyclient.h"
+
+using streamr::utils::toEthereumAddress;
+using streamr::utils::EthereumAddress;
+using streamr::utils::StreamPartID;
+using streamr::utils::StreamPartIDUtils;
+
+using namespace std::chrono_literals;
+
+uint64_t ProxyClient::newClient(std::string ownEthereumAddress, std::string streamPartId) const {
+    Error* errors = nullptr;
+    uint64_t numErrors = 0;
+    uint64_t clientHandle = proxyClientNew(&errors, &numErrors, ownEthereumAddress.c_str(), streamPartId.c_str());
+
+    if (numErrors > 0) {
+        return 0;
+    }
+    return clientHandle;
+}
+
+Result ProxyClient::deleteClient(uint64_t proxyClientHandle) const {
+    return Result{Result::ResultCode::Ok, ""};
+}
+
+Result ProxyClient::publish(uint64_t proxyClientHandle, std::string data) const {
+    Error* errors = nullptr;
+    uint64_t numErrors = 0;
+    proxyClientPublish(&errors, &numErrors, proxyClientHandle, data.c_str(), data.length());
+    std::this_thread::sleep_for(1s);
+    return Result{Result::ResultCode::Ok, ""};
+}
+
+uint64_t ProxyClient::connect(uint64_t proxyClientHandle, std::string websocketUrl,
+                              std::string ethereumAddress) const {
+    Error* errors = nullptr;
+    uint64_t numErrors = 0;
+    auto proxy = Proxy(websocketUrl.c_str(), ethereumAddress.c_str());
+    uint64_t result = proxyClientConnect(&errors, &numErrors, proxyClientHandle, &proxy, 1);
+    if (numErrors > 0) {
+        return 0;
+    }
+    return result;
+   
+}
---
+++ packages/streamr-libstreamrproxyclient/examples/ios/LocationShare/LocationShare/ProxyClient.hpp
@@ -0,0 +1,28 @@
+//
+//  ProxyClient.hpp
+//  LocationShare
+//
+//  Created by Santtu Rantanen on 25.8.2024.
+//
+
+#ifndef ProxyClient_hpp
+#define ProxyClient_hpp
+
+#define GLOG_USE_GLOG_EXPORT
+
+#include <string>
+#include "Result.hpp"
+#include "streamrproxyclient.h"
+
+class ProxyClient {
+private:
+    
+public:
+    ProxyClient() { }
+    uint64_t newClient(std::string ownEthereumAddress, std::string streamPartId) const;
+    Result deleteClient(uint64_t proxyClientHandle) const;
+    Result publish(uint64_t proxyClientHandle, std::string data) const;
+    uint64_t connect(uint64_t proxyClientHandle, std::string websocketUrl,
+                     std::string ethereumAddress) const;
+};
+#endif /* ProxyClient_hpp */
