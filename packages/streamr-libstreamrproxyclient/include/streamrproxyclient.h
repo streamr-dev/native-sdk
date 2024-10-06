38977a2fc1940246c941e0163d696f8ab0f0108b
Petri Savolainen <petri.savolainen@spaceify.net>
2024-10-06 23:54:27 +0300

    convert streamrproxyclient.h to pure C

--- packages/streamr-libstreamrproxyclient/include/streamrproxyclient.h
+++ packages/streamr-libstreamrproxyclient/include/streamrproxyclient.h
@@ -1,17 +1,21 @@
-#ifndef LIBSTREAMRPROXYCLIENT_HPP
-#define LIBSTREAMRPROXYCLIENT_HPP
+#ifndef LIBSTREAMRPROXYCLIENT_H
+#define LIBSTREAMRPROXYCLIENT_H
 
-#include <stdint.h> // NOLINT
+// NOLINTNEXTLINE
+#include <stdint.h>
 
 #if defined(__clang__) || defined(__GNUC__)
 #define SHARED_EXPORT __attribute__((visibility("default")))
 #define SHARED_LOCAL __attribute__((visibility("hidden")))
+#else
+#define SHARED_EXPORT
+#define SHARED_LOCAL
 #endif
 
-#if defined(__swift__)
-#define EXTERN_C
-#else
+#ifdef __cplusplus
 #define EXTERN_C extern "C"
+#else
+#define EXTERN_C
 #endif
 
 #define ERROR_INVALID_ETHEREUM_ADDRESS "INVALID_ETHEREUM_ADDRESS"
@@ -21,46 +25,48 @@
 #define ERROR_NO_PROXIES_DEFINED "NO_PROXIES_DEFINED"
 #define ERROR_PROXY_CONNECTION_FAILED "PROXY_CONNECTION_FAILED"
 #define ERROR_PROXY_BROADCAST_FAILED "PROXY_BROADCAST_FAILED"
-EXTERN_C const char* SHARED_EXPORT testRpc();
 
-// this file should be in C, not C++
+EXTERN_C SHARED_EXPORT const char* testRpc(void);
 
-static void __attribute__((constructor)) initialize();
+// NOLINTNEXTLINE
+static void __attribute__((constructor)) initialize(void);
 
-EXTERN_C struct SHARED_EXPORT Proxy {
+// NOLINTNEXTLINE
+typedef struct Proxy {
     const char* websocketUrl;
     const char* ethereumAddress;
-};
+} Proxy;
 
-EXTERN_C struct SHARED_EXPORT Error {
+// NOLINTNEXTLINE
+typedef struct Error {
     const char* message;
     const char* code;
-};
+} Error;
 
-EXTERN_C uint64_t SHARED_EXPORT proxyClientNew(
+EXTERN_C SHARED_EXPORT uint64_t proxyClientNew(
     Error** error,
     uint64_t* numErrors,
     const char* ownEthereumAddress,
     const char* streamPartId);
 
-EXTERN_C void SHARED_EXPORT
-proxyClientDelete(Error** errors, uint64_t* numErrors, uint64_t clientHandle);
+EXTERN_C SHARED_EXPORT void proxyClientDelete(
+    Error** errors, uint64_t* numErrors, uint64_t clientHandle);
 
-EXTERN_C uint64_t SHARED_EXPORT proxyClientConnect(
+EXTERN_C SHARED_EXPORT uint64_t proxyClientConnect(
     Error** errors,
     uint64_t* numErrors,
     uint64_t clientHandle,
     const Proxy* proxies,
     uint64_t numProxies);
 
-EXTERN_C void SHARED_EXPORT proxyClientDisconnect(
+EXTERN_C SHARED_EXPORT void proxyClientDisconnect(
     Error** errors, uint64_t* numErrors, uint64_t clientHandle);
 
-EXTERN_C void SHARED_EXPORT proxyClientPublish(
+EXTERN_C SHARED_EXPORT uint64_t proxyClientPublish(
     Error** errors,
     uint64_t* numErrors,
     uint64_t clientHandle,
     const char* content,
     uint64_t contentLength);
 
-#endif // LIBSTREAMRPROXYCLIENT_HPP
+#endif /* LIBSTREAMRPROXYCLIENT_H */
