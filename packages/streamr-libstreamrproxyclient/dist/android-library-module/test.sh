cfbd6c137acb339af0f6048f175068617bb40653
Santtu <santtu.rantanen@gmail.com>
2024-11-17 15:36:30 +0700

    Detailed test output

--- packages/streamr-libstreamrproxyclient/dist/android-library-module/test.sh
+++ packages/streamr-libstreamrproxyclient/dist/android-library-module/test.sh
@@ -2,6 +2,8 @@
 
 ./switch_build.sh test
 cd StreamrProxyClient
-./gradlew connectedAndroidTest
+./gradlew connectedDebugAndroidTest --info \
+    -Pandroid.testInstrumentationRunnerArguments.class=network.streamr.proxyclient.StreamrProxyClientTest \
+    --rerun-tasks
 cd ..
 ./switch_build.sh 
