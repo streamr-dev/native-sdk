341a03943338214a20cfafbef3ced40ef5bdbedd
Santtu <santtu.rantanen@gmail.com>
2024-11-17 17:17:58 +0700

    Example CPP application

--- packages/streamr-libstreamrproxyclient/dist/arm64-osx/include/streamrproxyclient.h
+++ packages/streamr-libstreamrproxyclient/dist/arm64-osx/include/streamrproxyclient.h
@@ -29,8 +29,25 @@
 EXTERN_C SHARED_EXPORT const char* testRpc(void);
 
 // NOLINTNEXTLINE
-static void __attribute__((constructor)) initialize(void);
+SHARED_EXPORT static void __attribute__((constructor)) initialize(void);
 
+
+/**
+ * @brief Initialize the library. This function is called automatically when the library is loaded,
+ * but can be called explicitly to force a re-initialization (e.g. after proxyClientCleanupLibrary() has been called).
+ */
+
+// NOLINTNEXTLINE
+EXTERN_C SHARED_EXPORT void proxyClientInitLibrary(void);
+
+/**
+ * @brief Cleanup the library. This function MUST be called before the program exits.
+ * Can be safely called multiple times. This is needed because the standard dynamic library
+ * destructor (__attribute__((destructor))) is called after static variables have already been destroyed, 
+ * which makes it impossible to clean up the other objects that depend on the static variables.
+ */
+// NOLINTNEXTLINE
+EXTERN_C SHARED_EXPORT void proxyClientCleanupLibrary(void);
 // NOLINTNEXTLINE
 typedef struct Proxy {
     const char* websocketUrl;
@@ -41,13 +58,32 @@ typedef struct Proxy {
 typedef struct Error {
     const char* message;
     const char* code;
+    const struct Proxy* proxy;
 } Error;
+
+// NOLINTNEXTLINE
+typedef struct ProxyResult {
+    Error* errors;
+    uint64_t numErrors;
+    Proxy* successful;
+    uint64_t numSuccessful;
+} ProxyResult;
+
+/**
+ * @brief Delete a ProxyResult. This method must be called after every call that
+ * returns a ProxyResult.
+ * @param proxyResult The ProxyResult to delete.
+ */
+
+EXTERN_C SHARED_EXPORT void proxyClientResultDelete(const ProxyResult* proxyResult);
 
 /**
  * @brief Create a new proxy client.
  *
- * @param errors The array of errors or NULL.
- * @param numErrors The number of errors.
+ * @param proxyResult Pointer in which ProxyResult will be stored. You MUST call
+ * proxyClientResultDelete() on this after the call returns. The resulting ProxyResult
+ * may only contain  "errors" - the "successful" and "numSuccessful" fields are
+ * unused.
  * @param ownEthereumAddress The Ethereum address of the client in format
  * 0x1234567890123456789012345678901234567890.
  * @param streamPartId The stream part id in format
@@ -56,27 +92,28 @@ typedef struct Error {
  */
 
 EXTERN_C SHARED_EXPORT uint64_t proxyClientNew(
-    Error** errors,
-    uint64_t* numErrors,
+    const ProxyResult** proxyResult,
     const char* ownEthereumAddress,
     const char* streamPartId);
 
 /**
  * @brief Delete a proxy client.
  *
- * @param errors The array of errors or NULL.
- * @param numErrors The number of errors.
+ * @param proxyResult Pointer in which ProxyResult will be stored. You MUST call
+ * proxyClientResultDelete() on this after the call returns. The resulting ProxyResult
+ * may only contain  "errors" - the "successful" and "numSuccessful" fields are
+ * unused.
  * @param clientHandle The client handle of the client to delete.
  */
 
 EXTERN_C SHARED_EXPORT void proxyClientDelete(
-    Error** errors, uint64_t* numErrors, uint64_t clientHandle);
+    const ProxyResult** proxyResult, uint64_t clientHandle);
 
 /**
  * @brief Connect a proxy client to a list of proxies.
  *
- * @param errors The array of errors or NULL.
- * @param numErrors The number of errors.
+ * @param proxyResult Pointer in which ProxyResult will be stored. You MUST call
+ * proxyClientResultDelete() on this after the call returns.
  * @param clientHandle The client handle of the client to connect.
  * @param proxies The array of proxies.
  * @param numProxies The number of proxies.
@@ -84,8 +121,7 @@ EXTERN_C SHARED_EXPORT void proxyClientDelete(
  */
 
 EXTERN_C SHARED_EXPORT uint64_t proxyClientConnect(
-    Error** errors,
-    uint64_t* numErrors,
+    const ProxyResult** proxyResult,
     uint64_t clientHandle,
     const Proxy* proxies,
     uint64_t numProxies);
@@ -93,29 +129,31 @@ EXTERN_C SHARED_EXPORT uint64_t proxyClientConnect(
 /**
  * @brief Disconnect a proxy client from all proxies.
  *
- * @param errors The array of errors or NULL.
- * @param numErrors The number of errors.
+ * @param proxyResult Pointer in which ProxyResult will be stored. You MUST call
+ * proxyClientResultDelete() on this after the call returns. The resulting ProxyResult
+ * may only contain  "errors" - the "successful" and "numSuccessful" fields are
+ * unused.
  * @param clientHandle The client handle of the client to disconnect.
- */
-
+ 
 EXTERN_C SHARED_EXPORT void proxyClientDisconnect(
-    Error** errors, uint64_t* numErrors, uint64_t clientHandle);
+    const ProxyResult** proxyResult, uint64_t clientHandle);
+*/
 
 /**
  * @brief Publish a message to the stream.
  *
- * @param errors The array of errors or NULL.
- * @param numErrors The number of errors.
+ * @param proxyResult Pointer in which ProxyResult will be stored. You MUST call
+ * proxyClientResultDelete() on this after the call returns.
  * @param clientHandle The client handle of the client to publish.
  * @param content The content to publish.
  * @param contentLength The length of the content.
- * @param ethereumPrivateKey as hex without 0x prefix (64 characters) or NULL if message should not be signed.
+ * @param ethereumPrivateKey as hex without 0x prefix (64 characters) or NULL if
+ * message should not be signed.
  * @return The number of proxies to which the message was published to.
  */
 
 EXTERN_C SHARED_EXPORT uint64_t proxyClientPublish(
-    Error** errors,
-    uint64_t* numErrors,
+    const ProxyResult** proxyResult,
     uint64_t clientHandle,
     const char* content,
     uint64_t contentLength,
