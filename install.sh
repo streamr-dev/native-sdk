0faee9ff5d2952e5900e38ec89e4bd5463871a6f
Santtu <santtu.rantanen@gmail.com>
2024-11-29 17:38:30 +0700

    androidbuild test

--- install.sh
+++ install.sh
@@ -99,10 +99,10 @@ else
     cd build && cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE .. && cmake --build . && cd ..
 fi
 
-if [ "$TARGET_TRIPLET" = "arm64-android" ]; then
-    mkdir -p packages/streamr-libstreamrproxyclient/dist/android-library-module/StreamrProxyClient/libs/arm64-v8a
-    cp packages/streamr-libstreamrproxyclient/build/libstreamrproxyclient.so packages/streamr-libstreamrproxyclient/dist/android-library-module/StreamrProxyClient/libs/arm64-v8a/libstreamrproxyclient.so
-    mkdir -p packages/streamr-libstreamrproxyclient/examples/android/LocationShare/StreamrProxyClient/libs/arm64-v8a 
-    cp packages/streamr-libstreamrproxyclient/build/libstreamrproxyclient.so packages/streamr-libstreamrproxyclient/examples/android/LocationShare/StreamrProxyClient/libs/arm64-v8a/libstreamrproxyclient.so
-fi
+#if [ "$TARGET_TRIPLET" = "arm64-android" ]; then
+#    mkdir -p packages/streamr-libstreamrproxyclient/dist/android-library-module/StreamrProxyClient/libs/arm64-v8a
+#    cp packages/streamr-libstreamrproxyclient/build/libstreamrproxyclient.so packages/streamr-libstreamrproxyclient/dist/android-library-module/StreamrProxyClient/libs/arm64-v8a/libstreamrproxyclient.so
+#    mkdir -p packages/streamr-libstreamrproxyclient/examples/android/LocationShare/StreamrProxyClient/libs/arm64-v8a 
+#    cp packages/streamr-libstreamrproxyclient/build/libstreamrproxyclient.so packages/streamr-libstreamrproxyclient/examples/android/LocationShare/StreamrProxyClient/libs/arm64-v8a/libstreamrproxyclient.so
+#fi
 
