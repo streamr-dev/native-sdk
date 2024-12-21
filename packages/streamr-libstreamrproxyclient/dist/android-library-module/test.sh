21250e4d59e7bde48b1eb65afb0fec58a89503b8
Santtu <santtu.rantanen@gmail.com>
2024-12-21 13:38:09 +0700

    Android async (Cororoutines) tests added

--- packages/streamr-libstreamrproxyclient/dist/android-library-module/test.sh
+++ packages/streamr-libstreamrproxyclient/dist/android-library-module/test.sh
@@ -3,7 +3,7 @@
 ./switch_build.sh test
 cd StreamrProxyClient
 ./gradlew connectedDebugAndroidTest --info \
-    -Pandroid.testInstrumentationRunnerArguments.class=network.streamr.proxyclient.StreamrProxyClientTest \
+    -Pandroid.testInstrumentationRunnerArguments.class=network.streamr.proxyclient.StreamrProxyClientTest,network.streamr.proxyclient.StreamrProxyClientCoroTest \
     --rerun-tasks
 cd ..
 ./switch_build.sh 
